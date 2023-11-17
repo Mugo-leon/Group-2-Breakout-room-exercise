db6cf11992845f5888970c228a6b772915b8dbe4
Leon <leon.muriuki@student.moringaschool.com>
2023-11-17 21:39:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -7,16 +7,19 @@ function solution(A, K) {
         // Get the value of the last element
         let lastNumber = A[lastIndex];
         
-        // Move the last element to the front
+        // Duplicates last element and moves it to the front
         A.unshift(lastNumber);
         
-        // Remove the last element from its original position
+        // Deletes last element in array. Also avoids duplicates of last element
         A.pop();
     }
     
     // Return the modified array
     return A;
 }
+
+//Example
 const A1 = [3, 8, 9, 7, 6];
 const K1 = 2;
+//running node index.js in terminal will return [ 7, 6, 3, 8, 9 ]
 console.log(solution(A1, K1));
